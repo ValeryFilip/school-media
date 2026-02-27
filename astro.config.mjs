394a362bceb0af4b15d7cd62c120df9e3f6d0190
School Media <valeryfilip96@gmail.com>
2026-02-27 23:42:08 +0700

    версия с одним цсс файлом, а не с чанками

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
 
     // Настройка Vite для поддержки алиасов
     vite: {
+        build: {
+            cssCodeSplit: false, // один общий CSS-файл вместо чанков
+        },
         resolve: {
             alias: {
                 '~': path.resolve(__dirname, './src'),
